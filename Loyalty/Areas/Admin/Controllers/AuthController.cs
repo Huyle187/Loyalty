98979b45e82f071e063591aa9ae68d932db17ca3
Huyle187 <lehaquanghuy1807@gmail.com>
2022-01-20 13:45:34 +0700

    Add project files.

---
+++ Loyalty/Areas/Admin/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Loyalty.Areas.Admin.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Admin/Auth
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection filed)
+        {
+            string user = filed["email"];
+            if (user != null)
+            {
+                return RedirectToAction("Index", "Dashboard");
+            }
+            return View(filed);
+        }
+    }
+}
