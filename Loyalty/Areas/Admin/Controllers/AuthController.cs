a96e34bfa4789004f242e6283e7cf44f97fd51b6
Huyle187 <lehaquanghuy1807@gmail.com>
2022-02-09 10:20:15 +0700

    Update login page and add dashboard page

--- Loyalty/Areas/Admin/Controllers/AuthController.cs
+++ Loyalty/Areas/Admin/Controllers/AuthController.cs
@@ -11,18 +11,75 @@ namespace Loyalty.Areas.Admin.Controllers
         // GET: Admin/Auth
         public ActionResult Login()
         {
+            ViewBag.StrError = "";
             return View();
         }
 
         [HttpPost]
         public ActionResult Login(FormCollection filed)
         {
+            string error = "";
             string user = filed["email"];
-            if (user != null)
+            string password = filed["password"];
+            if (user == "")
             {
-                return RedirectToAction("Index", "Dashboard");
+                if (password == "")
+                {
+                    error = "Email và mật khẩu không được để trống!";
+                }
+                else
+                {
+                    error = "Email không được để trống!";
+                }
             }
-            return View(filed);
+            else
+            {
+                if (password == "")
+                {
+                    error = "Mật khẩu không được để trống!";
+                }
+                else
+                {
+                    if (user == "admin@gmail.com")
+                    {
+                        if (password == "123456")
+                        {
+                            Session["UserAdmin"] = "admin@gmail.com";
+                            return RedirectToAction("Index", "Dashboard");
+                        }
+                        else
+                        {
+                            error = "Mật khẩu không chính xác!";
+                        }
+                    }
+                    else
+                    {
+                        if (password != "123456")
+                        {
+                            error = "Email và mật khẩu không chính xác!";
+                        }
+                        else
+                        {
+                            error = "Email không chính xác!";
+                        }
+                    }
+                }
+            }
+            ViewBag.StrError = "<div class = 'text-danger'>* " + error + "</div>";
+            return View();
+        }
+
+        // Admin/Auth/Forgot_password
+        public ActionResult Forgot_password()
+        {
+            ViewBag.StrError = "";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Forgot_password(FormCollection filed)
+        {
+            return View();
         }
     }
 }
--- Loyalty/Global.asax.cs
+++ Loyalty/Global.asax.cs
@@ -17,5 +17,10 @@ namespace Loyalty
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        protected void Session_Start()
+        {
+            Session["UserAdmin"] = "";
+        }
     }
 }
